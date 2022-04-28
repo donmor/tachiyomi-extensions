ac680f2b213b2c068d04bc1b43c8ad2fe9138c07
donmor <donmor3000@hotmail.com>
2022-04-28 17:01:23 +0800

    Update Bilibili.kt

--- src/all/bilibili/src/eu/kanade/tachiyomi/extension/all/bilibili/Bilibili.kt
+++ src/all/bilibili/src/eu/kanade/tachiyomi/extension/all/bilibili/Bilibili.kt
@@ -54,6 +54,7 @@ abstract class Bilibili(
     override fun headersBuilder(): Headers.Builder = Headers.Builder()
         .add("Accept", ACCEPT_JSON)
         .add("Origin", baseUrl)
+        .add("User-Agent", if(apiLang == "cn") "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36" else System.getProperty("http.agent")!!)
         .add("Referer", "$baseUrl/")
 
     private val apiLang: String = when (lang) {
